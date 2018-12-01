d5e4c2a612e48b1333859b24b4adb2448c57196b
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2018-12-01 22:55:02 +0100

    ProjectsInit

---
+++ SnoozyApp/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+	render() {
+		return (
+			<View>
+
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+
+});
