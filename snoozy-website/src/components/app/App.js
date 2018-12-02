0e78d47db8c8a9dc1cfed1810df45d13e4b04fb8
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2018-12-03 00:21:51 +0100

    BigClock

--- snoozy-website/src/components/app/App.js
+++ snoozy-website/src/components/app/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import BigClock from '../clock/BigClock';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				
+				<BigClock />
 			</div>
 		);
 	}
---
+++ snoozy-website/src/components/clock/BigClock.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BigClock extends React.Component {
+    render = () => {
+        return (
+            <div className='BigClock'>
+                <h1>10:34</h1>
+            </div>
+        )
+    }
+}
+
+export default BigClock;
