58e8e23ab4aebe8440c51594735fec0d096baa92
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2018-12-03 14:02:49 +0100

    AddedFirebase

---
+++ snoozy-website/src/components/notifications/NotificationsOverview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotificationsOverview extends React.Component {
+    render = () => {
+        return (
+            <div className='NotificationsOverview'>
+                
+            </div>
+        )
+    }
+}
+
+export default NotificationsOverview;
---
+++ snoozy-website/src/firebase/index.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
+import * as firebase_config from './firebase_config';
+
+const config = {
+  apiKey: firebase_config.API_KEY,
+  authDomain: firebase_config.DOMAIN,
+  databaseURL: firebase_config.DATABASE,
+  projectId: firebase_config.PROJECT_ID,
+  storageBucket: firebase_config.STORAGE,
+  messagingSenderId: firebase_config.MESSAGING
+};
+
+const app       = firebase.initializeApp(config);
+
+const database  = app.database();
+const storage   = app.storage();
+
+export { storage, firebase, database };
