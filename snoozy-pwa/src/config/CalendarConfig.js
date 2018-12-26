bd677938eab47cc78ed6493cd4e0125cd47ccc3f
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2018-12-26 14:21:42 +0100

    FixTwo

--- snoozy-pwa/src/config/CalendarConfig.js
+++
@@ -1,3 +0,0 @@
-export const CLIENT_ID       = 'test';
-export const DISCOVERY_DOCS  = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-export const SCOPES          = "https://www.googleapis.com/auth/calendar";
