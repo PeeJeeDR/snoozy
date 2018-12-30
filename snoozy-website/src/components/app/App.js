3104e5024cdfefec1761fce5f17e8a9445a22d6c
andrerobbe <mauritsrobbe@hotmail.com>
2018-12-30 20:39:20 +0100

    get Snoozy location

--- snoozy-website/src/components/app/App.js
+++ snoozy-website/src/components/app/App.js
@@ -6,8 +6,12 @@ import NotificationsOverview from '../notifications/NotificationsOverview';
 import Buzz from '../../assets/audio/buzz.mp3';
 import Sound from 'react-sound';
 import Overlay from '../overlay/Overlay';
+import $ from 'jquery'; 
+import axios from 'axios';
 
 const google  = window.google;
+const GOOGLE_MAP_KEY = 'AIzaSyDe2RQwYRxTmKXBFkr6d9oQqNOrT9K95hg';
+
 
 class App extends Component {
 	constructor (props) {
@@ -59,9 +63,14 @@ class App extends Component {
 		this.getAutoStatus();
 		this.getFirebaseData();
 		this.calculateTotalTime();
+		//this.getOrigin();
 	}
 
 	CheckAlarm = () => {
+		//console.log(this.state.totalTime.toLocaleTimeString());
+		//console.log(new Date().toLocaleTimeString());
+
+		if (this.state.totalTime.toLocaleTimeString() == new Date().toLocaleTimeString())
 		if (this.state.totalTime.toLocaleTimeString() === new Date().toLocaleTimeString())
 		{
 			this.setState({ alarmIsPlaying: true });
@@ -94,6 +103,74 @@ class App extends Component {
 			this.calculateTraffic(res.data().location)
 		})
 	}
+
+
+	//converts IP or geolocation to address
+	getAddress = (latitude, longitude) => {
+		/*$.ajax('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=' + GOOGLE_MAP_KEY)
+		.then(
+			function success (response) {
+				console.log('User\'s Address Data is ', response)
+			},
+			function fail (status) {
+				console.log('Request failed.  Returned status of', status)
+			}
+		)*/
+
+		axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=' + GOOGLE_MAP_KEY)
+		.then(res => {
+			console.log('User\'s Address Data is ', res);
+		})
+		.catch(err => {
+			console.log('Request failed.  Returned status of', err);
+		});
+	}
+	
+
+	getOriginWithIp = () => {
+		/*$.ajax('http://ip-api.com/json')
+		.then(
+			function success(response) {
+				console.log('User\'s Location Data is ', response);
+				console.log('User\'s Country ', response.country);
+				this.getAdress(response.lat, response.lon)
+			},
+
+			function fail(data, status) {
+				console.log('Request failed.  Returned status of ', status);
+			}
+		);*/
+
+		axios.get('http://ip-api.com/json')
+		.then(res => {
+				console.log('User\'s Location Data is ', res);
+				console.log('User\'s Country ', res.country);
+				this.getAdress(res.lat, res.lon);
+		})
+		.catch(err => {
+			console.log('Request failed.  Returned status of ', err);
+		});
+	}
+
+	getOrigin = () => {
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(
+				function success(position) {
+					console.log('latitude', position.coords.latitude, 'longitude', position.coords.longitude);
+					//this.getOriginWithIp();
+					this.getAddress(position.coords.latitude, position.coords.longitude);
+				},
+				function error(error_message) {
+					console.error('An error has occured while retrieving location', error_message);
+					this.getOriginWithIp();
+				}
+			);
+		} else {
+			console.log('geolocation is not enabled')
+			this.getOriginWithIp();
+		}
+	}
+
 
 	calculateTraffic = (from) => {
 		const origin                = 'Flierenbos 20, 2370 Arendonk';
@@ -159,6 +236,7 @@ class App extends Component {
 				<Overlay />
 		
 				<div className="all">
+					<button onClick={this.getOrigin}>getOriginBtn</button>
 					<BigClock />
 					<Alarm time={ this.state.totalTime }/>
 					<NotificationsOverview />
