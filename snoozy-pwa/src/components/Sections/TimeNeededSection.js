87789e4aed78634dd0609b4db129bcca7dd31d1f
andrerobbe <mauritsrobbe@hotmail.com>
2019-01-10 03:26:48 +0100

    timeNeeded.js

--- snoozy-pwa/src/components/Sections/TimeNeededSection.js
+++ snoozy-pwa/src/components/Sections/TimeNeededSection.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Paragraph from '../Paragraphs/Paragraph';
 import SmallSectionTitle from '../Titles/SmallSectionTitle';
 import OkButton from '../Buttons/OkButton';
+import * as ApiConfig from '../../config/ApiConfig';
+import { db } from '../../firebase/firebase';
 
+
+const snoozyRef = db.collection('snoozy').doc('user-data');
 
 class TimeNeededSection extends React.Component {
     constructor (props) {
@@ -10,6 +14,16 @@ class TimeNeededSection extends React.Component {
         this.state = {
             
         };
+    }
+
+    timeChanged = (e) => {
+        const time = e.target.value;
+
+        return;
+    }
+
+    timeSet = () => {
+        return;
     }
     
     render = () => {
@@ -27,14 +41,15 @@ class TimeNeededSection extends React.Component {
                 </Paragraph>
 
                 <div className='ManualBox'>
-                    <form className='inputContainer' onSubmit=''>
+                    <form className='inputContainer'>
                         <input 
                             name='time' 
                             type='time' 
                             min='00:00' 
                             max='23:59'
+                            value={ this.state.timeSet }
+                            onChange={ this.timeChanged }
                         />
-                        <OkButton/>
                     </form>
                 </div>
             </div>
